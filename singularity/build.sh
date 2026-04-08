debc8ed1cfcfbaae2042c206dcc9bea49b865486
Hariteja-jajula <hjajula@crimson.ua.edu>
2026-04-08 12:57:56 -0500

    Fix pip3 -> python3 -m pip, version-pin image tags from versions.yml, add v prefix for ciroh-ngen-image

--- singularity/build.sh
+++ singularity/build.sh
@@ -1,15 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+VERSIONS_FILE="${SCRIPT_DIR}/../versions.yml"
+
+DEPS_VERSION=$(grep 'datastream-deps' "$VERSIONS_FILE" | sed 's/.*: *"\(.*\)"/\1/')
+DS_VERSION=$(grep '^datastream:' "$VERSIONS_FILE" | sed 's/.*: *"\(.*\)"/\1/')
+
 echo "Building DataStreamCLI Singularity images..."
+echo "  datastream-deps version: ${DEPS_VERSION}"
+echo "  datastream version:      ${DS_VERSION}"
 
-# Main DataStreamCLI container
-singularity build datastream.sif datastream.def
+sed "s|^From: awiciroh/datastream:.*|From: awiciroh/datastream:${DS_VERSION}|" \
+    datastream.def > datastream_pinned.def
 
-# Additional workflow containers (built from Docker images)
-singularity build forcingprocessor.sif docker://awiciroh/forcingprocessor:latest
+singularity build --fakeroot datastream.sif datastream_pinned.def
+rm datastream_pinned.def
+
 singularity build merkdir.sif docker://zwills/merkdir:latest
-singularity build ngiabinabox.sif docker://awiciroh/ciroh-ngen-image:latest
+singularity build ngiabinabox.sif "docker://awiciroh/ciroh-ngen-image:v${DS_VERSION}"
 
 echo "All Singularity images built successfully."
-
